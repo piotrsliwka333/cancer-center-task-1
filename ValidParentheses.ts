e48423e85d50a55ea6507f6bddd2067f3d94f517
Peter Sliwka <piotrsliwka333@gmail.com>
2025-03-18 21:58:04 +0100

    master - update ValidParentheses.ts

--- ValidParentheses.ts
+++ ValidParentheses.ts
@@ -1,61 +1,64 @@
-const leftSideBrackets: string[] = ["(", "[", "{"];
-const matchers: Record<string, string> = { "(": ")", "[": "]", "{": "}" };
+const placeholderFoundLeftBracket: string = "-->";
+const placeholderFoundRightBracket: string = "<--";
+const leftSideBrackets: string[] = ["(", "[", "{", placeholderFoundLeftBracket];
+const matchers: Record<string, string> = { "(": ")", "[": "]", "{": "}", "-->": "<--" };
 
 const isValid = (s: string): boolean => {
   const splittedArray: string[] = s.split("");
   if (splittedArray.length < 2 || splittedArray.length % 2 !== 0) return false;
-  if (splittedArray.length > 1000) {
-    let leftSideBracketsCounter: number = 0;
-    let rightSideBracketsCounter: number = 0;
+  let pairsInside: number = 0;
+  let movesInRightDirectionCounter: number = 0;
+  let currentFirstElementToConsiderIndex: number = 0;
+  let comparingFromBack: number = splittedArray.length - 1;
+  let currentFirstElementToConsider: string = splittedArray[currentFirstElementToConsiderIndex];
 
-    for (let i = 0; i < splittedArray.length; i++) {
-      if (leftSideBrackets.includes(splittedArray[i])) {
-        leftSideBracketsCounter += 1;
-        continue;
-      }
-      rightSideBracketsCounter += 1;
+  for (let j = currentFirstElementToConsiderIndex + 1; j < splittedArray.length; j += 1) {
+    if (currentFirstElementToConsider === placeholderFoundRightBracket) {
+      currentFirstElementToConsiderIndex += 1;
+      currentFirstElementToConsider = splittedArray[currentFirstElementToConsiderIndex];
+      continue;
     }
-    return leftSideBracketsCounter === rightSideBracketsCounter;
-  }
-  for (let j = 1; j < splittedArray.length; j++) {
-    let movesInRightDirectionCounter = 0;
-    let counter = 1;
+    if (
+      matchers[currentFirstElementToConsider] === splittedArray[comparingFromBack] &&
+      matchers[currentFirstElementToConsider] !== splittedArray[j] &&
+      matchers[splittedArray[comparingFromBack - 1]] !== splittedArray[comparingFromBack] &&
+      comparingFromBack + currentFirstElementToConsiderIndex === splittedArray.length - 1
+    ) {
+      splittedArray[currentFirstElementToConsiderIndex] = placeholderFoundLeftBracket;
+      splittedArray[comparingFromBack] = placeholderFoundRightBracket;
 
-    while (leftSideBrackets.includes(splittedArray[counter])) {
+      currentFirstElementToConsiderIndex += 1;
+      currentFirstElementToConsider = splittedArray[currentFirstElementToConsiderIndex];
+      comparingFromBack -= 1;
+      continue;
+    }
+    if (matchers[splittedArray[j]] === splittedArray[j + 1] && splittedArray.length - 1 > j) {
+      splittedArray[j] = placeholderFoundLeftBracket;
+      splittedArray[j + 1] = placeholderFoundRightBracket;
+      pairsInside += 1;
+    }
+    if (leftSideBrackets.includes(splittedArray[j]) && !(matchers[splittedArray[j]] === splittedArray[j + 1])) {
       movesInRightDirectionCounter += 1;
-      counter += 1;
     }
     if (
-      matchers[splittedArray[j]] === splittedArray[j + 1] &&
-      splittedArray.length - 1 > j
+      matchers[currentFirstElementToConsider] === splittedArray[j] &&
+      currentFirstElementToConsiderIndex +
+        pairsInside * 2 +
+        movesInRightDirectionCounter +
+        movesInRightDirectionCounter +
+        1 ===
+        j
     ) {
-      splittedArray.splice(j + 1, 1);
-      splittedArray.splice(j, 1);
-      j = 0;
-      movesInRightDirectionCounter = 0;
-    }
-    if (
-      matchers[splittedArray[0]] === splittedArray[j] &&
-      movesInRightDirectionCounter + movesInRightDirectionCounter + 1 === j &&
-      leftSideBrackets.includes(splittedArray[j + 1])
-    ) {
-      splittedArray.splice(
-        movesInRightDirectionCounter + movesInRightDirectionCounter + 1,
-        1
-      );
-      splittedArray.splice(splittedArray.indexOf(splittedArray[0]), 1);
-      j = 0;
-      movesInRightDirectionCounter = 0;
-    }
-    if (
-      matchers[splittedArray[0]] === splittedArray[j] &&
-      splittedArray.length - 1 === j
-    ) {
-      splittedArray.splice(j, 1);
-      splittedArray.splice(splittedArray.indexOf(splittedArray[0]), 1);
-      j = 0;
+      splittedArray[currentFirstElementToConsiderIndex] = placeholderFoundLeftBracket;
+      splittedArray[j] = placeholderFoundRightBracket;
+      currentFirstElementToConsiderIndex += 1;
+      currentFirstElementToConsider = splittedArray[currentFirstElementToConsiderIndex];
+      j = currentFirstElementToConsiderIndex;
+      pairsInside = 0;
       movesInRightDirectionCounter = 0;
     }
   }
-  return splittedArray.length === 0;
+  return splittedArray.every(
+    (bracket: string) => bracket === placeholderFoundLeftBracket || bracket === placeholderFoundRightBracket
+  );
 };
